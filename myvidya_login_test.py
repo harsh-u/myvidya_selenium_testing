59bf7718c785777e13903dce0912dc0954b34d50
harsh <rajharsh662@gmail.com>
2021-11-12 13:54:45 +0530

    test report automated

--- myvidya_login_test.py
+++ myvidya_login_test.py
@@ -1,22 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-
-driver = webdriver.Firefox()
-driver.get("https://www.myvidya.org/")
-
-username = driver.find_element(By.ID, "userid")
-password = driver.find_element(By.ID, "password")
-
-username.send_keys("rajharsh662@gmail.com")
-password.send_keys("harsh")
-
-time.sleep(5)
+import unittest
+import HtmlTestRunner
+# import smtplib
 
 
-login = driver.find_element(By.XPATH, '//button[text()="LOG IN"]')
-login.click()
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.get("https://www.myvidya.org/")
 
-time.sleep(10)
+    def test_login(self):
+        t1 = time.time()
+        username = self.driver.find_element(By.ID, "userid")
+        password = self.driver.find_element(By.ID, "password")
 
-driver.close()
+        username.send_keys("rajharsh662@gmail.com")
+        password.send_keys("harsh")
+        t2 = time.time()
+
+        print("Username and password entering time: ", t2 - t1)
+        time.sleep(5)
+
+        login = self.driver.find_element(By.XPATH, '//button[text()="LOG IN"]')
+        login.click()
+
+        t3 = time.time() - 5
+        print("Time taken in Login: ", t3 - t2)
+        time.sleep(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+
+if __name__ == "__main__":
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='/home/harsh/PycharmProjects/selenium_testing/reports'))
+
+
+
